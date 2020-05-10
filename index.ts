78e50176aa8a8b1b481250334caf42a0c8db0eff
galicians <galicians@gmail.com>
2020-05-10 16:58:56 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -11,6 +11,6 @@ import {User} from "./User";
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 let user = new User('Pablo')
-//https://github.com/Microsoft/TypeScript/issues/11807
+
 console.log(user)
 
