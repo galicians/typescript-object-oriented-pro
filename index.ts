80620ad0f71c12522e640fe77d967f4251e2571d
galicians <galicians@gmail.com>
2020-05-10 12:46:57 +0100

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
+//https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+
 // Import stylesheets
 import './style.css';
+
+
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+
+// let user = new User();
