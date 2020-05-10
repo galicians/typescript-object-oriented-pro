38f7db235c855b3b05937756c4e0b5730ab805ca
galicians <galicians@gmail.com>
2020-05-10 17:33:02 +0100

    1 file modified, 1 added

---
+++ enums/SubscriptionType.enum.ts
@@ -0,0 +1,6 @@
+export enum SubscriptionType {
+  MONTHLY = 30,
+  ANNUAL = 365,
+  FREETRIAL = 10,
+  FREELIMITED = 0
+}
--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@
 
 // Import stylesheets
 import './style.css';
-import {User} from "./User";
+import { User } from './User';
+import { SubscriptionTypes } from './enums/SubscriptionTypes';
 
 
 
